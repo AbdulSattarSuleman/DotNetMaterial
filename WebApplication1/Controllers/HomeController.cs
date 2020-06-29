bd6addbbb9157225bafaef4cee53c620d54ffe68
Abdul Sattar Suleman <43110862+AbdulSattarSuleman@users.noreply.github.com>
2020-06-29 20:56:14 +0500

    first Class Material

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            List<Anylist> list = new List<Anylist>();
+
+            for(int i = 0; i < 15; i++)
+            {
+                list.Add(new Anylist()
+                {
+                    Id = i,
+                    Name = "Hammad" + i,
+                    FatherName = "Gabru" + i
+                });
+
+            }
+
+            return View(list);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
